8550aa1c96a26214c45543c85d5791ec7ee20204
Ted Bowman <ted+git@tedbow.com>
2022-10-04 19:22:59 -0400

    show assigned

--- content-script.js
+++ content-script.js
@@ -1,32 +1,74 @@
-var imgURL = chrome.runtime.getURL("images/get_started16.png");
-tabs = document.getElementById('tabs');
-tabList = tabs.getElementsByTagName('ul').item(0);
-var node = document.createElement('li');
-url = document.URL
-parts = url.split('/');
-lastPart = parts[parts.length - 1];
-parts = lastPart.split('#');
-issueId = parts[0];
-var jiraLink = document.createElement('a');
-var linkText = document.createTextNode("Find in jira");
-jiraLink.appendChild(linkText);
-jiraLink.title = "Search in jira";
-var search = `issues/${issueId}`;
-jiraLink.id = `jira-link-${issueId}`;
-jiraLink.href = `https://backlog.acquia.com/issues/?jql=text%20~%20%22${search}%22`;
-node.appendChild(jiraLink);
+
+var tabs = document.getElementById('tabs');
+if (tabs) {
+    var tabLists = tabs.getElementsByTagName('ul');
+    if (tabLists) {
+        var tabList = tabLists.item(0);
+        var node = document.createElement('li');
+        var url = document.URL
+        const regex = /https:\/\/www\.drupal\.org\/project\/automatic_updates\/issues\/.*/g;
+        if (url.match(regex)) {
+            var parts = url.split('/');
+            var lastPart = parts[parts.length - 1];
+            parts = lastPart.split('#');
+            var issueId = parts[0];
+            var jiraLink = document.createElement('a');
+            var linkText = document.createTextNode("Find in jira");
+            jiraLink.appendChild(linkText);
+            jiraLink.title = "Search in jira";
+            var search = `issues/${issueId}`;
+            jiraLink.id = `jira-link-${issueId}`;
+            jiraLink.href = `https://backlog.acquia.com/issues/?jql=text%20~%20%22${search}%22`;
+            node.appendChild(jiraLink);
+            tabList.appendChild(node);
+            getJiraIssue(issueId, updateMainJiraLink)
+        }
+    }
+}
+var links = document.querySelectorAll('a');
+
+links.forEach(function (link){
+    var href = link.href;
+    var regex = /\/project\/.*\/issues\/.*/g;
+    if (href.match(regex)) {
+        link.innerText += '***';
+    }
+});
+
+
+
 //node.appendChild(document.createTextNode(issueId));
 
-tabList.appendChild(node);
 
-console.log('what');
-chrome.runtime.sendMessage({call: 'fetchIssue', issue_id: issueId}, function(response) {
-    if (response.issue.issueCnt === 1) {
-        var link = document.getElementById(`jira-link-${issueId}`);
-        link.setAttribute('href', response.issue.url);
-        link.title = 'Open in Jira'
-        link.innerText = `Jira - ${response.issue.key} - assigned ${response.issue.assigned.displayName}`;
-        jiraLink.href = response.issue.url;
+
+function handleError(error) {
+    console.log(`Error: ${error}`);
+}
+function getJiraIssue(drupalIssueId, callback) {
+    return new Promise(function () {
+        chrome.runtime.sendMessage({call: 'fetchIssue', issue_id: issueId}, function(response) {
+            if (response.issue.issueCnt === 1) {
+                callback(response.issue);
+            }
+        });
+    }, handleError);
+}
+
+function updateMainJiraLink (jiraIssue){
+    var link = document.getElementById(`jira-link-${issueId}`);
+    link.setAttribute('href', jiraIssue.url);
+    link.title = 'Open in Jira'
+    link.innerText = `Jira - ${jiraIssue.key}`;
+    if (jiraIssue.assigned) {
+        link.innerText += ` - assigned ${jiraIssue.assigned.displayName}`;
     }
+    else {
+        link.innerText += ` - unassigned`;
+    }
+    jiraLink.href = jiraIssue.url;
+}
 
-});
+
+
+
+
