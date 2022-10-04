23b16e964262f064a147ee812ccff467a3f5836c
Ted Bowman <ted+git@tedbow.com>
2022-10-04 17:11:40 -0400

    workikng to create link

--- background.js
+++ background.js
@@ -13,13 +13,36 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
         alert("dhw");
     }
 })
-
+function parseIssueJson(text) {
+  data = JSON.parse(text);
+  issues = data.issues;
+  issueCnt = issues.length;
+  var response = {issueCnt: issueCnt};
+  if (issueCnt === 1) {
+      issue = issues[0];
+      response.url = `https://backlog.acquia.com/browse/${issue.key}`
+      response.key = issue.key;
+      response.assigned = issue.fields.assignee;
+  }
+  return response;
+}
 chrome.runtime.onMessage.addListener(
+
     function(request, sender, sendResponse) {
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension");
-        if (request.call === "fetchIssue")
+        if (request.call === "fetchIssue") {
+            var url = `https://backlog.acquia.com/rest/api/2/search?jql=description~%22issues/${request.issue_id}%22`;
+            fetch(url)
+                .then(response => response.text())
+                .then(text => sendResponse({issue: parseIssueJson(text)}))
+                //.then(price => sendResponse(price))
+                .catch(error => sendResponse({farewell: error}));
+
+            return true; // Will respond asynchronously.
             sendResponse({farewell: `id = ${request.issue_id}`});
+        }
+
     }
 );
--- content-script.js
+++ content-script.js
@@ -10,8 +10,9 @@ issueId = parts[0];
 var jiraLink = document.createElement('a');
 var linkText = document.createTextNode("Find in jira");
 jiraLink.appendChild(linkText);
-jiraLink.title = "my title text";
+jiraLink.title = "Search in jira";
 var search = `issues/${issueId}`;
+jiraLink.id = `jira-link-${issueId}`;
 jiraLink.href = `https://backlog.acquia.com/issues/?jql=text%20~%20%22${search}%22`;
 node.appendChild(jiraLink);
 //node.appendChild(document.createTextNode(issueId));
@@ -20,5 +21,12 @@ tabList.appendChild(node);
 
 console.log('what');
 chrome.runtime.sendMessage({call: 'fetchIssue', issue_id: issueId}, function(response) {
-    console.log(response.farewell);
+    if (response.issue.issueCnt === 1) {
+        var link = document.getElementById(`jira-link-${issueId}`);
+        link.setAttribute('href', response.issue.url);
+        link.title = 'Open in Jira'
+        link.innerText = `Jira - ${response.issue.key} - assigned ${response.issue.assigned.displayName}`;
+        jiraLink.href = response.issue.url;
+    }
+
 });
