0254ac02e49b662e25159d88dce0ec62a973664d
Ted Bowman <ted+git@tedbow.com>
2022-10-07 14:24:51 -0400

    jira not working

--- jira-content-script.js
+++ jira-content-script.js
@@ -4,39 +4,43 @@
 
     //.aui-header-primary .aui-nav
     document.querySelectorAll('.aui-header-primary .aui-nav').forEach(function (el){
-        const link = document.createElement('a');
-        link.setAttribute('href', '*');
+        const link = document.createElement('div');
+        //link.setAttribute('href', '*');
         link.innerText = "DRUPAL";
-        
+        link.onclick = function() {triggerDrupalIntegration()};
         const li = document.createElement('li');
         li.appendChild(link);
         el.appendChild(li);
     });
-    let ids = [];
-    document.querySelectorAll('div[data-issue-id]').forEach(function (div) {
-       ids.push(div.getAttribute('data-issue-id'));
-    });
 
-    function createDrupalLink(issue) {
-        document.querySelectorAll(`div[data-issue-id="${issue.id}"]`).forEach(function (div) {
-            if (issue.drupalUrl) {
-                let link = document.createElement("a");
-                link.setAttribute("href", issue.drupalUrl);
-                link.title = "Open on Drupal.org";
-                link.innerText = `Open on Drupal.org`;
-            }
 
+    function triggerDrupalIntegration() {
+        let ids = [];
+        document.querySelectorAll('div[data-issue-id]').forEach(function (div) {
+            ids.push(div.getAttribute('data-issue-id'));
         });
-    }
+        function createDrupalLink(issue) {
+            document.querySelectorAll(`div[data-issue-id="${issue.id}"]`).forEach(function (div) {
+                if (issue.drupalUrl) {
+                    let link = document.createElement("a");
+                    link.setAttribute("href", issue.drupalUrl);
+                    link.title = "Open on Drupal.org";
+                    link.innerText = `Open on Drupal.org`;
+                }
 
-/*    chrome.runtime.sendMessage(
-        { call: "fetchJIraIssuesByIds", ids: ids },
-        function (response) {
-            response.issues.forEach(function (issue) {
-                createDrupalLink(issue);
             });
         }
-    );*/
+
+        chrome.runtime.sendMessage(
+            { call: "fetchJIraIssuesByIds", ids: ids },
+            function (response) {
+                response.issues.forEach(function (issue) {
+                    createDrupalLink(issue);
+                });
+            }
+        );
+    }
+
 
 
 })();
