a830e3533a69e419496a8f0cecd385aadef08d08
Ted Bowman <ted+git@tedbow.com>
2022-10-07 12:23:50 -0400

    use string key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const jiraConfig = {
-  jira_create_url:
+  "jira_create_url":
     "https://backlog.acquia.com/secure/CloneIssueDetails!default.jspa?id=12348176",
-  jira_base_url: "https://backlog.acquia.com/"
+  "jira_base_url": "https://backlog.acquia.com/"
 };
-//export default jiraConfig;
+export { jiraConfig };
